c5222e638e4fc49f6ccdfc9dfc6e6533e952dbfd
jep2032 <jep2032@gmail.com>
2021-10-19 20:12:13 -0400

    Removed Live Camera Feed

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
       <body class="App-body">
 
         <div class="row">
-          <div class="col-7">
-            <img src="TestImage.jpg" width="90%"></img>
+          <div class="col-3">
           </div>
-          <div class="col-5">
+          <div class="col-6">
 
             <div class="row">
-
-              Move Camera:
+              <div class="col-12">
+                Move Camera:
+              </div>
             </div>
             <div class="row">
               <div class="col-3">
@@ -37,21 +37,61 @@ function App() {
                 <button type="button" class="btn btn-color col-10">Right</button>
               </div>
             </div>
-
             <div class="row">
-              Zoom:
+              &nbsp;
+            </div>
+            <div class="row">
+              <div class="col-12">
+                Zoom:
+              </div>
             </div>
 
             <div class="row">
               <div class="col-1">1x</div>
-            <div class="col-10">
-              <input type="range" class="form-range"></input>
+              <div class="col-10">
+                <input type="range" class="form-range" min="0" max="100"></input>
               </div>
               <div class="col-1">4x</div>
             </div>
-
             <div class="row">
-</div>
+              &nbsp;
+            </div>
+            <div class="row">
+              <div class="col-3">
+                Mask:
+              </div>
+              <div class="col-3">
+                <label class="switch switch-flat switch-format">
+                  <input class="switch-input" type="checkbox" />
+                  <span class="switch-label" data-on="On" data-off="Off"></span>
+                  <span class="switch-handle"></span>
+                </label>
+              </div>
+              <div class="col-3">
+                Crosshair:
+              </div>
+              <div class="col-3">
+                <label class="switch switch-flat switch-format">
+                  <input class="switch-input" type="checkbox" />
+                  <span class="switch-label" data-on="On" data-off="Off"></span>
+                  <span class="switch-handle"></span>
+                </label>
+              </div>
+              <div class="row">
+                &nbsp;
+              </div>
+              <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-6">
+                  <button type="button" class="btn btn-color col-10">Calibrate Crank When Idle</button>
+                </div>
+                <div class="col-3">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3">
           </div>
         </div>
       </body>
